09571e59c5b2c183d5a63786c54614c5537fdcf6
Lebid_Dmytro <lebedd99@gmail.com>
2025-06-17 17:35:17 +0300

    add views logic permissions

--- cinema_up_project/apps.py
+++ cinema_up_project/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CinemaUpProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cinema_up_project'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cinema_up_project"
--- cinema_up_project/views.py
+++ cinema_up_project/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .serializers import *
+from .permissions import IsAdminOrReadOnly, IsOwner
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class PerformanceViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+    permission_classes = [AllowAny]
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
